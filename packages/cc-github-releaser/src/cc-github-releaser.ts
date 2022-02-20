4c1ae9eb2ae13fa229747a18ad06cc67ab99b02c
Michael M <mullinsmikey@users.noreply.github.com>
2022-02-20 17:33:38 +0400

    feat: refactor to reduce dependencies

--- packages/cc-github-releaser/src/cc-github-releaser.ts
+++ packages/cc-github-releaser/src/cc-github-releaser.ts
@@ -2,82 +2,67 @@
 
 import ccGithubReleaser from './runner';
 import { ChangelogOpts, GitRawCommitsOpts, ImportedConfig, ParserOpts, TemplateContext, WriterOpts } from './types';
-import meow from 'meow';
 import { resolve } from 'path';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
 
-const cli = meow({
-    help: `
-Usage      conventional-github-releaser
-Example    conventional-github-releaser -p angular
-
-Options
-  -t, --token       Your GitHub auth token
-
-  -p, --preset      Name of the preset you want to use. Must be one of the following:
-                    angular, atom, codemirror, ember, eslint, express, jquery, jscs or jshint
-                    Default: angular
-
-  -k, --pkg         A filepath of where your package.json is located
-                    Default is the closest package.json from cwd
-
-  -r, --releases    How many releases to be generated from the latest
-                    If 0, the whole changelog will be regenerated and
-                    the output file will be overwritten
-                    Default: 1
-
-  -f, --prefix      Release name prefix
-                    Default: ''
-
-  -n, --config      A filepath of your config script
-
-  -c, --context     A filepath of a javascript that is used to define template variables
-
-  -d, --draft       Publish a draft release
-                    Default: false
-  `,
-    flags: {
-        token: {
-            alias: 't',
-            default:
-                process.env['GITHUB_TOKEN'] ||
-                process.env['GH_TOKEN'] ||
-                process.env['CONVENTIONAL_GITHUB_RELEASER_TOKEN'] ||
-                '_',
-            type: 'string',
-        },
-        preset: {
-            alias: 'p',
-            default: 'angular',
-            type: 'string',
-        },
-        pkg: {
-            alias: 'k',
-            type: 'string',
-        },
-        releases: {
-            alias: 'r',
-            default: 1,
-            type: 'number',
-        },
-        prefix: {
-            alias: 'f',
-            type: 'string',
-        },
-        config: {
-            alias: 'n',
-            type: 'string',
-        },
-        context: {
-            alias: 'c',
-            type: 'string',
-        },
-        draft: {
-            alias: 'd',
-            default: false,
-            type: 'boolean',
-        },
-    },
-});
+const cli = yargs(hideBin(process.argv))
+    .option('token', {
+        alias: 't',
+        type: 'string',
+        description: 'Your GitHub auth token',
+    })
+    .option('preset', {
+        alias: 'p',
+        default: 'angular',
+        type: 'string',
+        description:
+            `Name of the preset you want to use. Must be one of the following: ` +
+            `angular, atom, codemirror, ember, eslint, express, jquery, jscs or jshint`,
+    })
+    .option('pkg', {
+        alias: 'k',
+        type: 'string',
+        description: `A filepath of where your package.json is located. Default is the closest package.json from cwd`,
+    })
+    .option('releases', {
+        alias: 'r',
+        default: 1,
+        type: 'number',
+        description:
+            `How many releases to be generated from the latest. ` +
+            `If 0, the whole changelog will be regenerated and the output file will be overwritten`,
+    })
+    .option('prefix', {
+        alias: 'f',
+        default: '',
+        type: 'string',
+        description: `Release name prefix`,
+    })
+    .option('config', {
+        alias: 'n',
+        type: 'string',
+        description: 'A filepath of your config script',
+    })
+    .option('context', {
+        alias: 'c',
+        type: 'string',
+        description: 'A filepath of a javascript that is used to define template variables',
+    })
+    .option('draft', {
+        alias: 'd',
+        default: false,
+        type: 'boolean',
+        description: 'Publish a draft release',
+    })
+    .option('verbose', {
+        alias: 'v',
+        default: false,
+        type: 'boolean',
+        description: 'Verbose output. Use this for debugging',
+    })
+    .usage('Usage: cc-github-releaser [options]')
+    .parse();
 
 let config: ImportedConfig = {};
 let context: TemplateContext | undefined;
@@ -86,11 +71,11 @@ let parserOpts: ParserOpts | undefined;
 let writerOpts: WriterOpts | undefined;
 
 try {
-    if (cli.flags.context) {
-        context = require(resolve(process.cwd(), cli.flags.context));
+    if (cli.context) {
+        context = require(resolve(process.cwd(), cli.context));
     }
-    if (cli.flags.config) {
-        config = require(resolve(process.cwd(), cli.flags.config));
+    if (cli.config) {
+        config = require(resolve(process.cwd(), cli.config));
     }
     if (config.gitRawCommitsOpts) {
         gitRawCommitsOpts = config.gitRawCommitsOpts;
@@ -107,27 +92,32 @@ try {
 }
 
 const changelogOpts: ChangelogOpts = {
-    preset: cli.flags.preset,
-    pkg: { path: cli.flags.pkg },
-    releaseCount: cli.flags.releases,
-    releasePrefix: cli.flags.prefix,
-    draft: cli.flags.draft,
+    preset: cli.preset,
+    pkg: { path: cli.pkg },
+    releaseCount: cli.releases,
+    releasePrefix: cli.prefix,
+    draft: cli.draft,
 };
 
 ccGithubReleaser({
-    token: cli.flags.token,
+    token:
+        cli.token ||
+        process.env['GITHUB_TOKEN'] ||
+        process.env['GH_TOKEN'] ||
+        process.env['CONVENTIONAL_GITHUB_RELEASER_TOKEN'] ||
+        '',
     changelogOpts,
     context,
     gitRawCommitsOpts,
     parserOpts,
     writerOpts,
-    userCb: function (err, data) {
+    userCb: (err, data) => {
         if (err) {
             console.error(err.toString());
             process.exit(1);
         }
-        // if (flags.verbose) {
-        //     console.log(data)
-        // }
+        if (cli.verbose) {
+            console.log(data);
+        }
     },
 });
